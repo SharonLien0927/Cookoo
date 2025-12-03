a1184d8b06ff1119baf3de15849cf297134e964e
SharonLien0927 <sharon1062001@gmail.com>
2025-12-03 23:48:27 +0800

    router: switch to hash history for GitHub Pages compatibility

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../pages/Home.vue'
 import Calendar from '../pages/Calendar.vue'
 import Ingredients from '../pages/Ingredients.vue'
@@ -22,7 +22,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes,
 })
 
